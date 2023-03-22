eadc982e3457b2dc4616ad08ce8e01bc2f4eaf5e
Sean Eric Thomas <set@seanericthomas.com>
2023-03-22 12:57:00 -0700

    Add rudimentary styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import Header from './components/Header';
 import RouteSwitch from './components/Router';
 import Footer from './components/Footer';
 
+import './styles/reset.css';
+import './styles/style.css';
+
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <RouteSwitch />
       <Footer />
