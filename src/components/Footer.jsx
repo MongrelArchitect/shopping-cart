85e890adfc851188f9bef1f36ae22644f7677736
Sean Eric Thomas <set@seanericthomas.com>
2023-03-28 16:44:37 -0700

    Style and add links to "Footer" section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,41 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div>
-      <h3>Footer</h3>
+    <div className="footer">
+      <ul>
+        <li>
+          Designed by
+          {' '}
+          <a
+            target="_blank"
+            href="http://www.seanericthomas.com"
+            rel="noreferrer"
+          >
+            Sean Eric Thomas
+          </a>
+          .
+        </li>
+        <li>
+          Products from
+          {' '}
+          <a target="_blank" href="https://fakestoreapi.com/" rel="noreferrer">
+            Fake Store API
+          </a>
+          .
+        </li>
+        <li>
+          Home image from
+          {' '}
+          <a
+            target="_blank"
+            href="https://unsplash.com/photos/c9FQyqIECds"
+            rel="noreferrer"
+          >
+            Unsplash
+          </a>
+          .
+        </li>
+      </ul>
     </div>
   );
 }
