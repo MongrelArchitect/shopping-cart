f9c95ca1f824e2a6e89e31a507cf54d09d94f252
Sean Eric Thomas <set@seanericthomas.com>
2023-03-30 12:45:43 -0700

    Test "Header" component

--- src/components/Header.test.jsx
+++ src/components/Header.test.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
-describe('Cart Quantity', () => {
-  it('displays total quantity of items in cart', () => {
-    const mockCart = {
+describe('Header component', () => {
+  test('Renders correctly with zero cart items', () => {
+    const { container } = render(
+      <BrowserRouter>
+        <Header cart={{}} />
+      </BrowserRouter>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  test('Shows correct number of items in cart', () => {
+    const fakeCart = {
       1: 7,
       2: 1,
-      9: 12,
+      4: 3,
     };
 
-    render(<Header cart={mockCart} />);
-    const cartQuantity = screen.getByText(/\(\d+ items?\)/g);
+    render(
+      <BrowserRouter>
+        <Header cart={fakeCart} />
+      </BrowserRouter>,
+    );
+
+    const cartItems = screen.getByText(/\(\d* item(s)\)/);
+    expect(cartItems.textContent).toMatch('(11 items)');
+  });
+
+  test('Style links according to router path', () => {
+    render(
+      <MemoryRouter initialEntries={['/shop']}>
+        <Header cart={{}} />
+      </MemoryRouter>,
+    );
+
+    const shopLink = screen.getByText('Shop');
+    const homeLink = screen.getByText('Home');
+    const cartLink = screen.getByText('Cart');
+    expect(shopLink.classList.contains('active')).toBe(true);
+    expect(homeLink.classList.contains('active')).toBe(false);
+    expect(cartLink.classList.contains('active')).toBe(false);
   });
 });
